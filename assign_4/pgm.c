77731a8419db416074fc806a51c168a7ee5ad92e
subratamaity <subratamaity645@gmail.com>
2015-08-04 16:22:35 +0530

    incomplete assignment 5

--- assign_4/pgm.c
+++ assign_4/pgm.c
@@ -4,9 +4,13 @@ int main()
 {             
 
     FILE *fp;	
-    	
+     FILE *fp1;	
      char ch;
+     char a[];
+     char *b,size[50];
     fp=fopen("LenaASCII.pgm","r");
+    fp=fopen("1.txt","w");
+
 
     if(fp==NULL)
     {
@@ -21,7 +25,11 @@ int main()
         }
     }
 
+    fscanf(fp,"%s \n",a);
+    fprintf(fp1,"%s \n",a);
+    getline(&b,&size,fp);
+    fprintf(fp1,"%s \n",)
     fclose(fp);
-
-    return 0;  
+    fclose(fp1);
+	return 0;  
 }
