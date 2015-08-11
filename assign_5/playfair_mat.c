affc4e49aec9271d432f1cf838301f7740f0ea7d
student <student@student-HP-EliteDesk-800-G1-TWR.(none)>
2015-08-11 21:59:25 +0530

    complete assignment

--- assign_5/playfair_mat.c
+++ assign_5/playfair_mat.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-	int i,l,j;
-	int k=0;
-	char key[50]={0};
-	printf("enter the key text \n");
-	scanf("%s",key);
-	char str[26]={0};
-	// scanf("%s",str);
-	l=strlen(key);
-	char matrix[5][5]={{0}};
-	for(i=0;i<4;i++){
-  		for(j=0;j<4;j++){
-  			if(k<l)
-  			matrix[i][j]=key[k++];
-  			
- 		} 
-	}
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			printf("%c ",matrix[i][j]);
-		}
-
-	}
-
+int i,l,j,k,a;
+char key[50]={0};
+char matrix[5][5]={{0}};
+char *p;
+p=(char*)matrix;
+int str[26]={0};
+printf("enter the key text \n");
+scanf("%s",key);
+l=strlen(key);
+for(k=0;k<l;k++){
+a=key[k]-'a';
+if(str[a] < 1 && key[k] != 'i' ){
+++str[a];
+*p=key[k];
+p++;
+}
+}
+for(i=0;i<26;i++){
+if(str[i]!=1 && i != 'i' -'a'){
+*p=i + 'a';
+p++;
+}
+}
+for(i=0;i<5;i++){
+for(j=0;j<5;j++){
+printf("%c ",matrix[i][j]);
+}
+printf("\n");
+}
 }
